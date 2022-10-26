cc5b9369231c2b1e39aec6a92b9575aa79c423a8
Eduard Ghenea <87914375+Edi013@users.noreply.github.com>
2022-10-26 17:11:49 +0300

    Deleting the less important commentaries

--- Bridges-MuchiiCritice.cpp
+++ Bridges-MuchiiCritice.cpp
@@ -1,5 +1,3 @@
-﻿// Algoritmul de parcurgere in adancime (DFS). Reprezentarea grafului se face cu matrice de adiacenta.
-
 #include <stdio.h>
 #include <string.h>
 
@@ -7,12 +5,12 @@
 
 #define NMAX 100
 
-char vecin[NMAX][NMAX]; // Matricea de adiacenta a grafului
+char vecin[NMAX][NMAX]; 
 
-char visit[NMAX];      // visit[k] = 1 daca nodul k a fost vizitat si 0 in caz contrar
+char visit[NMAX];      
 
-int trace[NMAX];       // tablou ce pastreaza nodurile in ordinea in care sunt intalnite la parcurgerea in latime
-int kth;               // numarul de elemente vizitate
+int trace[NMAX];       
+int kth;               
 
 int prenum[NMAX];
 int low[NMAX];
@@ -21,13 +19,13 @@ int low[NMAX];
 void dfs(int n, short u) {
     int v;
 
-    visit[u] = 1;        // marcheaza nodul u ca fiind vizitat
+    visit[u] = 1;        
     prenum[u] = kth;
-    trace[kth++] = u;    // adauga nodul u in tabloul ce contine lista nodurilor deja vizitate //trace ==  prenum
+    trace[kth++] = u;    
 
-    for (v = 0; v < n; v++) {       // pentru toate nodurile grafului
-        if ((vecin[u][v] == 1) && !visit[v]) {  // verifica daca u si v sunt vecine si daca v nu a fost vizitat
-            dfs(n, v);                  // apeleaza recursiv dfs pentru nodul v
+    for (v = 0; v < n; v++) {       
+        if ((vecin[u][v] == 1) && !visit[v]) {  
+            dfs(n, v);                 
         }
     }
 
@@ -49,25 +47,22 @@ int main() {
     }
 
     for (i = 0; i < m; i++) {
-        fscanf(fin, "%d %d", &u, &v);  // citeste o pereche de noduri adiacente
+        fscanf(fin, "%d %d", &u, &v); 
 
-        u--;                  // scade 1 din valorile nodurilor
-        v--;                  // transforma nodurile din intervalul 1...n in 0...(n-1)
-        vecin[u][v] = 1;      // marcheaza faptul ca u si v sunt adiacente
+        u--;                 
+        v--;                 
+        vecin[u][v] = 1;     
         vecin[v][u] = 1;
     }
 
-    dfs(n, start);          // porneste din start sa viziteze nodurile folosind strategia DFS
-
-    // afiseaza in ordine nodurile vizitate
+    dfs(n, start);        
+    
     for (i = 0; i < kth; i++) {
         fprintf(fout, "%d ", trace[i] + 1);
     }
 
     int traceLength = kth;
     
-    //calculam low pt fiecare, luate pe inversul parcurgerii,  din trace le luam.
-
     kth--;
     for (; kth >= 0; kth-- ) {
         int nodCurent = trace[kth];
@@ -75,34 +70,29 @@ int main() {
         int minPrenumeVecini = prenum[nodCurent];
         low[nodCurent] = prenum[nodCurent]; //#1
         int nrVecini = 0;
-        for (v = 0; v < n; v++) {// v = nodIncercat
+        for (v = 0; v < n; v++) {
             nrVecini++;
-           // prenum [copil] > prenum[parinte] && max( de prenume dintre vecini.)
                 if (vecin[nodCurent][v] == 1)
                 {
                     if (prenum[v] < prenum[nodCurent]) // e muchie de intoarcere sau parinte
                     {
-                        //pt a gasii tatal cautam prenumele maxim dintre cei mai mici decat el
+                        //pt a gasii tatal cautam prenum maxim pentru nodurile cu prenum mai mic decat cel curent
                         if (maxPrenumeVecini < prenum[v])
                             maxPrenumeVecini = prenum[v];
-                        //pt a gasii o muchie de intoarcere cautam prenumele minim dintre cei mai mici decat el cu cond sa fie diferit de prenumele maxim si de prenum[nodCurent]
+                        //pt a gasii o muchie de intoarcere cautam prenumele minim dintre cu prenum mai mic decat cel curent,
+                        //cu conditia sa fie diferit de maxPrenumeVecini si de prenum[nodCurent]- conditie la linia 96
                         if (minPrenumeVecini > prenum[v])
                             minPrenumeVecini = prenum[v];
                     }
                     else                          // e copil
                         if (low[nodCurent] > low[v] )
                             low[nodCurent] = low[v];
-                
-                }
-                
-
+                }               
         }
         //muchia de intoarcere o verificam dupa ce a trecut prin toti vecinii si daca  exista cel putin un vecin,  un minim diferit de maxim, min dif de prenum[nodCurent]
             if (nrVecini != 0 && minPrenumeVecini != maxPrenumeVecini && minPrenumeVecini != prenum[nodCurent] )// inseamna ca avem muchie de intoarcere;
                 if (low[nodCurent] > minPrenumeVecini)
-                    low[nodCurent] = minPrenumeVecini;
-
-       
+                    low[nodCurent] = minPrenumeVecini;      
     }
 
     printf("\nPrenum ");
@@ -119,6 +109,7 @@ int main() {
     //verificam conditia  prenum[parinte] < low[copil] ESTE Bridge : NU ESTE
     for(int i = traceLength-1; i >= 0; i--)
      {
+        // nodCurent are viziune de parinte, parcurgand, cu forul de mai jos, copiii parintelui
         int nodCurent = trace[i];
         int maxPrenumeVecini = -1;
         int nodCopil = -1;
@@ -128,19 +119,13 @@ int main() {
                     if (prenum[v] > prenum[nodCurent]) // e copil
                     {
                         nodCopil = v;
-                        //pt a gasii tatal cautam prenumele maxim dintre cei mai mici decat el
-                        if (prenum[nodCurent] < low[nodCopil]) // e muchie critica  // nodCurent e viziune de parinte. low de copil trb
+                        if (prenum[nodCurent] < low[nodCopil]) // e muchie critica  
                             printf("%d %d - bridge\n", nodCurent + 1, nodCopil + 1);
-
                     }
                 }
             }
-
-            
-
     }
     
-
     fclose(fin);
     fclose(fout);
 
